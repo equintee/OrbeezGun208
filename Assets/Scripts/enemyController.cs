04784f9e5e6843e4c49cb1282159ef1bbbc819f1
equintee <employes23@gmail.com>
2022-07-27 14:55:07 +0300

    EU

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -7,6 +7,16 @@ public class enemyController : MonoBehaviour
 
     public float moveSpeed;
     public float decrementSpeed;
+
+    private void Start()
+    {
+        foreach(Transform enemy in transform)
+        {
+            enemy.parent = null;
+            enemy.localScale = Vector3.one;
+            enemy.parent = transform;
+        }
+    }
     public float getMoveSpeed()
     {
         return moveSpeed;
